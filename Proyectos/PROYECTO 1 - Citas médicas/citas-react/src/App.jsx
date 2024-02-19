837061ff3c448281dfe2c3c0819452c4d2c569ec
Noel Sariñena Varela <noelsava25@gmail.com>
2024-02-19 22:11:34 +0100

    Proyecto 1: Initial commit

---
+++ Proyectos/PROYECTO 1 - Citas médicas/citas-react/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+    <> // Debe tener 
+      <div>
+        <h1>Hola mundo</h1>
+        <p>Un párrafo</p>
+      </div>
+      <div>
+        <h1>Hola mundo</h1>
+        <p>Un párrafo</p>
+      </div>
+    </>
+  )
+}
+
+export default App
