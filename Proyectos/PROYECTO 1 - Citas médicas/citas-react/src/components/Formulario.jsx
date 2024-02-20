c0a0fc426000f0cb905302c0234684da2210e167
Noel Sariñena Varela <noelsava25@gmail.com>
2024-02-20 22:17:24 +0100

    Proyecto 1: useState

--- Proyectos/PROYECTO 1 - Citas médicas/citas-react/src/components/Formulario.jsx
+++ Proyectos/PROYECTO 1 - Citas médicas/citas-react/src/components/Formulario.jsx
@@ -1,15 +1,44 @@
 import { useState, useEffect } from 'react'
+import Error from './Error';
 
-const Formulario = () => {
+const Formulario = ({ pacientes, setPacientes }) => {
     const [nombre, setNombre] = useState('');
     const [propietario, setPropietario] = useState('');
     const [email, setEmail] = useState('');
     const [fecha, setFecha] = useState('');
     const [sintomas, setSintomas] = useState('');
 
+    const [error, setError] = useState(false);
+
     const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log('enviando formulario...')
+        e.preventDefault();
+
+        // Validación del Formulario
+        if ([nombre, propietario, email, fecha, sintomas].includes('')) {
+            console.log('Hay almenos un campo vacío');
+            setError(true);
+            return;
+        }
+
+        setError(false);
+
+        // Objeto de Paciente
+        const objetoPaciente = {
+            nombre, 
+            propietario, 
+            email, 
+            fecha, 
+            sintomas
+        }
+
+        setPacientes([...pacientes, objetoPaciente]);
+
+        // Reiniciar el form
+        setNombre('');
+        setPropietario('');
+        setEmail('');
+        setFecha('');
+        setSintomas('');
     }
 
     return (
@@ -25,6 +54,7 @@ const Formulario = () => {
                 onSubmit={handleSubmit}
                 className='bg-white shadow-md rounded-lg py-10 px-5 mb-10'
             >
+                { error && <Error mensaje='Todos los campos son obligatorios.'/>}
                 <div className='mb-5'>
                     <label htmlFor="mascota" className='block text-gray-700 uppercase font-bold'>
                         Nombre Mascota
--- Proyectos/PROYECTO 1 - Citas médicas/citas-react/src/components/ListadoPacientes.jsx
+++ Proyectos/PROYECTO 1 - Citas médicas/citas-react/src/components/ListadoPacientes.jsx
@@ -2,21 +2,22 @@ import Paciente from './Paciente'
 
 const ListadoPacientes = () => {
   return (
-    <div className='md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll'>
+    <div className='md:w-1/2 lg:3/5'>
       <h2 className='font-black text-3xl text-center'>Listado Pacientes</h2>
 
-      <p className='text-xl mt-5 mb-10 text-center'>
+      <p className='text-xl mt-5 text-center'>
         Administra tus {''}
-        <span className='text-indigo-600 font-bold'>Pacientas y Citas</span>
+        <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
       </p>
 
-      <Paciente />
-      <Paciente />
-      <Paciente />
-      <Paciente />
-      <Paciente />
-      <Paciente />
-
+      <div className='md:h-screen mt-10 overflow-y-scroll'>
+        <Paciente />
+        <Paciente />
+        <Paciente />
+        <Paciente />
+        <Paciente />
+        <Paciente />
+      </div>
     </div>
   )
 }
