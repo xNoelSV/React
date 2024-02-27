6b8582b3e6d6b2524afbca5d2f127608b0b1e958
Noel Sariñena Varela <noelsava25@gmail.com>
2024-02-27 21:30:36 +0100

    Proyecto 2: Inicialización

---
+++ Proyectos/PROYECTO 2 - Control de gastos/control-gastos/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
+
+const App = () => {
+
+  const [presupuesto, setPresupuesto] = useState(0);
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+
+
+  return (
+    <div>
+      <Header
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
+      />
+
+      {isValidPresupuesto && (
+        <div className='nuevo-gasto'>
+          <img src={IconoNuevoGasto} alt="Icono Nuevo Gasto" />
+        </div>
+      )}
+
+    </div>
+  )
+}
+
+export default App
+
